2d874c3fc42f7691d2bfc2bac604aa754f313ccb
gabrieldigitalsales-del <gabriel.digitalsales@gmail.com>
2026-03-26 17:32:21 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ window.STORE_CONFIG = {
   storeName: 'Mobiliadora Cristelli',
   footerText: '© {year} Mobiliadora Cristelli',
   wifi: {
-    ssid: 'COLOQUE_O_NOME_DA_REDE_AQUI',
-    password: 'COLOQUE_A_SENHA_AQUI',
+    ssid: 'Cristelli_',
+    password: 'cliente01',
 
   },
   links: {
